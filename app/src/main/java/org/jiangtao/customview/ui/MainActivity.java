4c5853565c53a2525e1fe8b15b303e0235049e6e
kevin <jiangtao103cp@163.com>
2016-05-17 00:12:47 +0800

    init

---
+++ app/src/main/java/org/jiangtao/customview/ui/MainActivity.java
@@ -0,0 +1,13 @@
+package org.jiangtao.customview.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.jiangtao.customview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
