aba11b55fc6d996e6c017a2c2546468e02a803dd
kevin <jiangtao103cp@163.com>
2016-05-19 22:47:10 +0800

    fix

--- app/src/main/java/org/jiangtao/customview/ui/MainActivity.java
+++ app/src/main/java/org/jiangtao/customview/ui/MainActivity.java
@@ -1,13 +1,24 @@
 package org.jiangtao.customview.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import org.jiangtao.customview.R;
+import org.jiangtao.customview.widget.CustomViewCircle;
+import org.jiangtao.customview.widget.CustomViewLine;
 
 public class MainActivity extends AppCompatActivity {
+
+  @InjectView(R.id.custom_line) CustomViewLine customLine;
+  @InjectView(R.id.custom_circle) CustomViewCircle customCircle;
+  @InjectView(R.id.toolbar) Toolbar toolbar;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.inject(this);
   }
 }
