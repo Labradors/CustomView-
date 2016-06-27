0ac5611eb1f142fa9042ba82ac8a4d7b84dcae48
Kevin <jiangtao103cp@163.com>
2016-06-27 22:16:08 +0800

    add calcultor

--- app/src/main/java/org/jiangtao/customview/ui/MainActivity.java
+++ app/src/main/java/org/jiangtao/customview/ui/MainActivity.java
@@ -1,24 +1,35 @@
 package org.jiangtao.customview.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import org.jiangtao.customview.R;
 import org.jiangtao.customview.widget.CustomViewCircle;
 import org.jiangtao.customview.widget.CustomViewLine;
 
 public class MainActivity extends AppCompatActivity {
 
-  @InjectView(R.id.custom_line) CustomViewLine customLine;
-  @InjectView(R.id.custom_circle) CustomViewCircle customCircle;
   @InjectView(R.id.toolbar) Toolbar toolbar;
+  @InjectView(R.id.btn_calculater) Button btnCalculater;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.inject(this);
+  }
+
+  @OnClick(R.id.btn_calculater) public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.btn_calculater:
+        Intent intent = new Intent(MainActivity.this, ClockViewActivity.class);
+        startActivity(intent);
+        break;
+    }
   }
 }
